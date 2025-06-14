4d5958ca69fb932d91c469bea05a75d987deea4e
Jack Jiang <578091306@qq.com>
2025-06-14 08:32:49 +0800

    update

--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -6,16 +6,16 @@ import styles from './styles.module.less';
 const NavMenu: FC = () => {
   return (
     <div className={styles.menus}>
-       {navs.map(nav => {
-          return (
-            <Link
-              key={nav.path}
-              to={nav.path}
-            >
-              {nav.title}
-            </Link>
-          );
-        })}
+      {navs.filter(e => !e.rel).map(nav => {
+        return (
+          <Link
+            key={nav.path}
+            to={nav.path}
+          >
+            {nav.title}
+          </Link>
+        );
+      })}
     </div>
   );
 }
