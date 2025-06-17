260abebd2510a3c29e8bfdf08968b7c5eeafa4b2
Jack <578091306@qq.com>
2025-06-17 20:53:32 +0800

    update

--- src/pages/WorkflowDetail/index.tsx
+++ src/pages/WorkflowDetail/index.tsx
@@ -1,12 +1,16 @@
 import { Button, Card } from 'antd';
+import cloneDeep from 'clone-deep';
 import { useRef, lazy, Suspense } from 'react';
 import { useMemoizedFn } from 'ahooks';
 import { PageContainer } from '@ant-design/pro-components';
 import { EditFilled } from '@ant-design/icons';
 import { Spinner } from '@/components';
+import type { Edge } from '@xyflow/react';
 import type { AiWorkFlowInstance } from '@/components/AiWorkFlow/types';
+import type { NodeType } from '@/components/AiWorkFlow/types';
 import styles from './styles.module.less';
 import { nodes, edges } from './mock';
+import { transformInputFormData } from '@/components/AiWorkFlow/components/DynamicForm/helper';
 
 const AiWorkFlow = lazy(() => import('@/components/AiWorkFlow'));
 
@@ -14,8 +18,40 @@ function WorkflowDetail() {
   const flowInstance = useRef<AiWorkFlowInstance>(null);
 
   const getDataHandler = useMemoizedFn(() => {
-    const result = flowInstance.current?.getData();
-    console.log(result);
+    const result = flowInstance.current?.getData()!;
+    const { nodes: stepNodes = [], edges: stepEdges = [] } = result;
+
+    const deepData = (node: NodeType, edges: Edge[]) => {
+      const nodeId = node.id;
+      const values = node.data.values;
+      const formData = values.formData;
+
+      const edge = edges.find(edge => {
+        return edge.source === nodeId;
+      });
+
+      return {
+        ...values,
+        uid: nodeId,
+        nextId: edge?.target,
+        formData: transformInputFormData(formData)
+      }
+    };
+
+    const stpes = stepNodes.map(node=> {
+      return deepData(node, stepEdges); 
+    });
+
+
+    const data = stpes.map(step => {
+      const stepData = step.stepRealData;
+      const childNodes = stepData?.nodes || [];
+      const childEdges = stepData?.edges || [];
+
+    });
+
+    // console.log('Workflow Data:', data);
+
   });
 
   return (
