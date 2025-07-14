65a7d6dea4bb5d8fe40ac73163062b0935457390
Jack Jiang <578091306@qq.com>
2025-07-14 18:02:01 +0800

    build

--- dist/assets/workflow-CFsmYefh.js
+++ dist/assets/workflow-CFsmYefh.js
@@ -1,4 +1,4 @@
-var u=Object.defineProperty;var d=(i,e,t)=>e in i?u(i,e,{enumerable:!0,configurable:!0,writable:!0,value:t}):i[e]=t;var r=(i,e,t)=>d(i,typeof e!="symbol"?e+"":e,t);import{x as w}from"./libs-DVH06oKf.js";class a{constructor(e,t){r(this,"code");r(this,"message");this.code=`${e}`,this.message=t}}const S="https://e7zmwbmq2c.execute-api.us-west-2.amazonaws.com/beta/core",f="oNaPRVvvsj1cJivZNpP7P2giiX8Dof4M94mghktw",c="openai",m=[S,c].join("/");class l{constructor(e){r(this,"request");r(this,"namespace");r(this,"baseUrl",m);r(this,"headers");r(this,"requestBefore",async e=>{if(this.headers){const t=await this.headers(e.headers);e.headers=Object.assign(e.headers,t)}return e});r(this,"parseSuccess",async e=>{const t=e.data;let p="10001",s=t.error;return s?(typeof s!="string"&&(p=s.errorType,s=s.errorMessage),Promise.reject(new a(p,s))):Promise.resolve(t.query?t:t.data)});r(this,"parseError",e=>{if(e.response)return this.parseSuccess(e.response);{const t=e.status,p=e.message;return Promise.reject(new a(t,p))}});const{apiKey:t=f}=e;e.baseUrl&&(this.baseUrl=e.baseUrl),this.request=w.create({baseURL:this.baseUrl,headers:{"x-api-key":t}}),this.headers=e.headers,this.namespace=e.namespace,this.send=this.send.bind(this),this.graphql=this.graphql.bind(this),this.request.interceptors.request.use(this.requestBefore),this.request.interceptors.response.use(this.parseSuccess,this.parseError)}send(e){return this.request.post(`/${this.namespace}`,e)}graphql(e){return this.send(e)}}new l({namespace:"ai_agent_build_graphql_query"});const o=new l({namespace:"ai_agent_core_graphql"}),n=`
+var u=Object.defineProperty;var d=(s,e,t)=>e in s?u(s,e,{enumerable:!0,configurable:!0,writable:!0,value:t}):s[e]=t;var r=(s,e,t)=>d(s,typeof e!="symbol"?e+"":e,t);import{x as w}from"./libs-DVH06oKf.js";class a{constructor(e,t){r(this,"code");r(this,"message");this.code=`${e}`,this.message=t}}const S="https://yuqnw5pscc.execute-api.us-west-2.amazonaws.com/beta/core",f="DwW7QneAAH28UR6NspPYw4dU1PEhcNxl5M9H2xkT",c="openai",h=[S,c].join("/");class l{constructor(e){r(this,"request");r(this,"namespace");r(this,"baseUrl",h);r(this,"headers");r(this,"requestBefore",async e=>{if(this.headers){const t=await this.headers(e.headers);e.headers=Object.assign(e.headers,t)}return e});r(this,"parseSuccess",async e=>{const t=e.data;let p="10001",i=t.error;return i?(typeof i!="string"&&(p=i.errorType,i=i.errorMessage),Promise.reject(new a(p,i))):Promise.resolve(t.query?t:t.data)});r(this,"parseError",e=>{if(e.response)return this.parseSuccess(e.response);{const t=e.status,p=e.message;return Promise.reject(new a(t,p))}});const{apiKey:t=f}=e;e.baseUrl&&(this.baseUrl=e.baseUrl),this.request=w.create({baseURL:this.baseUrl,headers:{"x-api-key":t}}),this.headers=e.headers,this.namespace=e.namespace,this.send=this.send.bind(this),this.graphql=this.graphql.bind(this),this.request.interceptors.request.use(this.requestBefore),this.request.interceptors.response.use(this.parseSuccess,this.parseError)}send(e){return this.request.post(`/${this.namespace}`,e)}graphql(e){return this.send(e)}}new l({namespace:"ai_agent_build_graphql_query"});const n=new l({namespace:"ai_agent_core_graphql"}),o=`
   endpointId 
   flowSnippetVersionUuid 
   flowSnippetUuid 
@@ -11,7 +11,7 @@ var u=Object.defineProperty;var d=(i,e,t)=>e in i?u(i,e,{enumerable:!0,configura
   updatedBy 
   createdAt 
   updatedAt 
-`,h=`
+`,U=`
   query flowSnippetList(
     $pageNumber: Int, 
     $limit: Int, 
@@ -34,11 +34,11 @@ var u=Object.defineProperty;var d=(i,e,t)=>e in i?u(i,e,{enumerable:!0,configura
       pageNumber 
       total 
       flowSnippetList { 
-        ${n}
+        ${o}
       }
     }
   }
-`,U=`
+`,m=`
   query flowSnippet(
     $flowSnippetVersionUuid: String,
     $flowSnippetUuid: String
@@ -47,7 +47,7 @@ var u=Object.defineProperty;var d=(i,e,t)=>e in i?u(i,e,{enumerable:!0,configura
       flowSnippetVersionUuid: $flowSnippetVersionUuid, 
       flowSnippetUuid: $flowSnippetUuid
     ) {
-      ${n}
+      ${o}
     }
   }
 `,$=`
@@ -74,7 +74,7 @@ var u=Object.defineProperty;var d=(i,e,t)=>e in i?u(i,e,{enumerable:!0,configura
       updatedBy: $updatedBy
     ) {
       flowSnippet { 
-        ${n}
+        ${o}
       }
     }
   }
@@ -84,4 +84,4 @@ var u=Object.defineProperty;var d=(i,e,t)=>e in i?u(i,e,{enumerable:!0,configura
       ok
     }
   } 
-`,V=i=>o.graphql({query:h,variables:i}),b=i=>o.graphql({query:U,variables:i}),N=i=>o.graphql({query:$,variables:i}),x=i=>o.graphql({query:g,variables:i});export{b as a,x as d,N as i,V as q};
+`,V=s=>n.graphql({query:U,variables:s}),b=s=>n.graphql({query:m,variables:s}),N=s=>n.graphql({query:$,variables:s}),x=s=>n.graphql({query:g,variables:s});export{b as a,x as d,N as i,V as q};
