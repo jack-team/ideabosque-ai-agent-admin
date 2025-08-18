5daf6155b614f28cea52db0206248a192e342ea9
Jack Jiang <578091306@qq.com>
2025-08-18 10:32:07 +0800

    update

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,1 +1,2 @@
 export const primaryColor = '#303030';
+export const borderColor = '#8A8A8A'
--- src/theme/config.ts
+++ src/theme/config.ts
@@ -1,10 +1,31 @@
 import type { ThemeConfig } from 'antd';
-import { primaryColor } from './colors';
+import { primaryColor, borderColor } from './colors';
 
 export const themeConfigs: ThemeConfig = {
   token: {
     colorLink: primaryColor,
     colorPrimary: primaryColor,
     colorBgContainer: '#fff',
+  },
+  components: {
+    Input: {
+      borderRadius: 8,
+      colorBorder: borderColor,
+      colorTextDisabled: '#D1D5DB',
+      colorBgContainerDisabled: 'transparent'
+    },
+    Select: {
+      borderRadius: 8,
+      colorBorder: borderColor,
+      multipleItemBg: '#E3E3E3',
+      colorTextDisabled: '#D1D5DB',
+      multipleSelectorBgDisabled: '#333',
+      colorBgContainerDisabled: 'transparent',
+      multipleItemColorDisabled: '#303030'
+    },
+    Form: {
+      labelFontSize: 13,
+      labelColor: '#303030'
+    }
   }
 };
