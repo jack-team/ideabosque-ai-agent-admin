0dd537cf73577f941517eb40f8ec64ddce0a0a42
Jack Jiang <578091306@qq.com>
2025-08-19 15:24:36 +0800

    update

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,7 +1,6 @@
 import type { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import ShopifyNavMenu from '@/components/ShopifyNavMenu';
-import NavMenu from '@/components/NavMenu';
 import Auth from '@/components/Auth';
 import './styles.less';
 
@@ -9,7 +8,6 @@ const BaseLayout: FC = () => {
   return (
     <div className="base_layout">
       <ShopifyNavMenu />
-      <NavMenu></NavMenu>
       <Auth>
         <div className="base_layout_body">
           <Outlet />
