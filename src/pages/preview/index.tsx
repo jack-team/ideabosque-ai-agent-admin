e2f1749f5dcd9e9314847a27f3bafce5ed1f9311
Jack Jiang <578091306@qq.com>
2025-09-30 10:06:36 +0800

    update

--- src/pages/preview/index.tsx
+++ src/pages/preview/index.tsx
@@ -1,9 +1,9 @@
 import qs from 'qs';
-import { type FC, useMemo } from 'react';
+import { type FC, useMemo, useRef } from 'react';
 import ShopifyButton from '@/components/ShopifyButton'
+import { PageContainer } from '@ant-design/pro-components';
 import { useSafeState, useMemoizedFn, useUpdateEffect } from 'ahooks';
 import { ProForm, ProFormSelect, ProFormDependency } from '@ant-design/pro-components'
-import { PageContainer } from '@ant-design/pro-components';
 import SpinBox from '@/components/SpinBox';
 import { useCoordinationList, useEndpointId } from './hooks';
 import styles from './styles.module.less';
@@ -13,14 +13,20 @@ type FormDataType = {
   agent: string;
 }
 
+type OptionType = {
+  label: string;
+  value: string;
+}
+
 const aibotUrl = 'https://ideabosque-ai-chat.pages.dev';
 
 const Preview: FC = () => {
   const [form] = ProForm.useForm();
   const endpointId = useEndpointId();
+  const agentsOptionsRef = useRef<OptionType[]>([]);
   const [iframeLoading, setIframeLoading] = useSafeState(false);
-  const [formData, setFormData] = useSafeState<FormDataType | undefined>();
   const { data = [], loading: dataLoading } = useCoordinationList();
+  const [formData, setFormData] = useSafeState<FormDataType | undefined>();
 
   const urlSearch = useMemo(() => {
     return qs.stringify({ ...formData, mode: 'preview' });
@@ -28,16 +34,14 @@ const Preview: FC = () => {
 
   const handleSubmit = useMemoizedFn(async () => {
     const values = await form.validateFields();
-    setFormData({ ...values, endpointId });
+    const agentId = values.agent;
+    const options = agentsOptionsRef.current;
+    const agent = options.find(e => e.value === agentId);
+    setFormData({ ...values, endpointId, agentName: agent?.label });
   });
 
-  const onIframeLoadStart = useMemoizedFn(() => {
-    setIframeLoading(true)
-  });
-
-  const onIframeLoadEnd = useMemoizedFn(() => {
-    setIframeLoading(false)
-  });
+  const onIframeLoadStart = useMemoizedFn(() => setIframeLoading(true));
+  const onIframeLoadEnd = useMemoizedFn(() => setIframeLoading(false));
 
   useUpdateEffect(onIframeLoadStart, [urlSearch]);
 
@@ -47,65 +51,6 @@ const Preview: FC = () => {
       className="shopify full-screen"
     >
       <div className={styles.container}>
-        <div className={styles.contrl}>
-          <ProForm
-            form={form}
-            submitter={false}
-            className={styles.form}
-          >
-            <ProFormSelect
-              label="Coordination"
-              name="coordination"
-              options={data}
-              fieldProps={{
-                loading: dataLoading,
-                onChange: () => {
-                  form.resetFields(['agent']);
-                }
-              }}
-              rules={[
-                { required: true }
-              ]}
-            />
-            <ProFormDependency name={['coordination']}>
-              {({ coordination }) => {
-                if (!coordination) return null;
-
-                const item = data.find(e => {
-                  return e.value === coordination;
-                });
-
-                const options = item?.agents.map(item => {
-                  return {
-                    label: item.agent_uuid,
-                    value: item.agent_uuid
-                  }
-                });
-
-                return (
-                  <ProFormSelect
-                    label="Agent"
-                    name="agent"
-                    options={options}
-                    rules={[
-                      { required: true }
-                    ]}
-                  />
-                )
-              }}
-            </ProFormDependency>
-            <div className={styles.submitter}>
-              <ShopifyButton
-                block
-                type="primary"
-                size="large"
-                onClick={handleSubmit}
-              >
-                Apply
-              </ShopifyButton>
-            </div>
-          </ProForm>
-        </div>
         <div className={styles.preview}>
           <SpinBox loading={iframeLoading}>
             <div className={styles.preview_box}>
@@ -124,6 +69,75 @@ const Preview: FC = () => {
               )}
             </div>
           </SpinBox>
+        </div>
+        <div className={styles.contrl}>
+          <ProForm
+            form={form}
+            submitter={false}
+            className={styles.form}
+          >
+            <ProFormSelect
+              label="Coordination"
+              name="coordination"
+              options={data}
+              fieldProps={{
+                placement: 'bottomRight',
+                popupMatchSelectWidth: false,
+                loading: dataLoading,
+                onChange: () => {
+                  form.resetFields(['agent']);
+                }
+              }}
+              rules={[
+                { required: true }
+              ]}
+            />
+            <ProFormDependency name={['coordination']}>
+              {({ coordination }) => {
+                if (!coordination) return null;
+
+                const agents = data.find(e => {
+                  return e.value === coordination;
+                })?.agents || [];
+
+                const options = agents.map(item => {
+                  const label = item.agent_name
+                  const value = item.agent_uuid;
+                  return {
+                    label: label || value,
+                    value: value
+                  }
+                });
+
+                agentsOptionsRef.current = options;
+
+                return (
+                  <ProFormSelect
+                    label="Agent"
+                    name="agent"
+                    options={options}
+                    fieldProps={{
+                      placement: 'bottomLeft',
+                      popupMatchSelectWidth: false,
+                    }}
+                    rules={[
+                      { required: true }
+                    ]}
+                  />
+                )
+              }}
+            </ProFormDependency>
+            <div className={styles.submitter}>
+              <ShopifyButton
+                block
+                type="primary"
+                size="large"
+                onClick={handleSubmit}
+              >
+                Apply
+              </ShopifyButton>
+            </div>
+          </ProForm>
         </div>
       </div>
     </PageContainer>
