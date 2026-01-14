ca724e041e12f748fb0d8265446bacc7f292c8af
Jack Jiang <578091306@qq.com>
2026-01-14 17:32:26 +0800

    feat: 修改 auth url

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,10 +1,10 @@
 import qs from 'qs';
-import { appInstallUrl } from '@/env';
+import { appInstallUrl, endpointId } from '@/env';
 
 // 安装 App
 export const appInstallApi = async (params: Record<string, any>) => {
   const query = qs.stringify(params, { addQueryPrefix: true });
-  const result = await fetch(`${appInstallUrl}/beta/core/openai/app_check${query}`);
+  const result = await fetch(`${appInstallUrl}/beta/core/${endpointId}/app_check${query}`);
   const json = await result.json();
   return json as { authed: boolean };
 }
