513bebcf34aeb43954d3d0489e153aecd95ef394
Jack Jiang <578091306@qq.com>
2026-01-14 22:15:01 +0800

    feat: 取消安装流程

--- src/components/AppWrapper/index.tsx
+++ src/components/AppWrapper/index.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import { type FC, type PropsWithChildren } from 'react';
 import { useMount } from 'ahooks';
 import Spinner from '@/components/Spinner'
@@ -7,11 +8,11 @@ const AppWrapper: FC<PropsWithChildren> = (props) => {
   const loading = useAppInstallModel(s => s.loading);
   const appIntsall = useAppInstallModel(s => s.appIntsall);
 
-  useMount(appIntsall);
+  // useMount(appIntsall);
 
-  if (loading) {
-    return <Spinner className="spinner" />;
-  }
+  // if (loading) {
+  //   return <Spinner className="spinner" />;
+  // }
 
   return props.children;
 }
