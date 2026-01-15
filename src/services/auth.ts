93e2b7561d92ca91cd467d8cb3057e1650273737
Jack Jiang <578091306@qq.com>
2026-01-15 19:13:56 +0800

    feat：修改请求方式

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,11 @@
 import qs from 'qs';
+import axios from 'axios';
 import { installBaseUrl } from '@/env';
 
 
 // 安装 App
 export const appInstallApi = async (params: Record<string, any>) => {
   const query = qs.stringify(params, { addQueryPrefix: true });
-  const result = await fetch(`${installBaseUrl}/app_check${query}`);
-  const json = await result.json();
-  return json as { authed: boolean };
+  const result = await axios.get<{ authed: boolean }>(`${installBaseUrl}/app_check${query}`);
+  return result.data;
 }
